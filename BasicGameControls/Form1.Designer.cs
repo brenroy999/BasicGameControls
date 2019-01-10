d4e79fad79a82bac17eb6fe2b554f776b5cb3cb5
brenroy999 <brenroy999@ed.amdsb.ca>
2019-01-10 14:58:25 -0500

    Add project files.

---
+++ BasicGameControls/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace BasicGameControls
+{
+    partial class Demo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // Demo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = global::BasicGameControls.Properties.Resources.WORLD;
+            this.ClientSize = new System.Drawing.Size(240, 160);
+            this.Name = "Demo";
+            this.Text = "Pokemon Demo";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ BasicGameControls/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicGameControls
+{
+    public partial class Demo : Form
+    {
+        bool moving = false;
+        string direction = "down";
+        string leftRight = "left";
+        public Demo()
+        {
+            InitializeComponent();
+            gameTimer.Enabled = true;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    {
+                        direction = "up";
+                        if (moving = false)
+                        {
+                            moving = true;
+                        }
+                    }
+                    break;
+                case Keys.A:
+                    {
+                        direction = "right";
+                        if (moving = false)
+                        {
+                            moving = true;
+                        }
+                    }
+                    break;
+                case Keys.S:
+                    {
+                        direction = "down";
+                        if (moving = false)
+                        {
+                            moving = true;
+                        }
+                    }
+                    break;
+                case Keys.D:
+                    {
+                        direction = "left";
+                        if (moving = false)
+                        {
+                            moving = true;
+                        }
+                    }
+                    break;
+            }
+
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            if (moving)
+            {
+                switch (direction)
+                {
+                    case "down":
+                        break;
+                    case "up":
+                        break;
+                    case "left":
+                        break;
+                    case "right":
+                        break;
+                }
+            }
+            Refresh();
+        }
+    }
+}
