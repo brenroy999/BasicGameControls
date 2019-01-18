27470c20ea4f3b4c959cf96ab84f470e3b353fae
brenroy999 <brenroy999@ed.amdsb.ca>
2019-01-18 15:09:55 -0500

    -Minor changes

--- BasicGameControls/Battle.Designer.cs
+++ BasicGameControls/Battle.Designer.cs
@@ -41,6 +41,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.label1.Location = new System.Drawing.Point(89, 86);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
--- BasicGameControls/Properties/Resources.Designer.cs
+++ BasicGameControls/Properties/Resources.Designer.cs
@@ -231,6 +231,16 @@ namespace BasicGameControls.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] pokemon_emerald_pro {
+            get {
+                object obj = ResourceManager.GetObject("pokemon_emerald_pro", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap ralts_b {
